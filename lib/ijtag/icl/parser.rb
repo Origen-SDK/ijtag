2282b61a545c60c3959a1c27a3468bea3cf823cc
Stephen McGinty <stephen.mcginty@freescale.com>
2015-10-21 09:26:44 -0500

    Lint cleanup

--- lib/ijtag/icl/parser.rb
+++ lib/ijtag/icl/parser.rb
@@ -3,16 +3,15 @@ module IJTAG
   module ICL
     # Responsible for parsing an 1687 ICL file to an AST
     class Parser
-
       def self.parse(data)
         tree = parser.parse(data)
 
         # If the AST is nil then there was an error during parsing
         # we need to report a simple error message to help the user
-        if(tree.nil?)
+        if tree.nil?
           parser.failure_reason =~ /^(Expected .+) (after|at)/m
           @last_error_msg = []
-          @last_error_msg << "#{$1.gsub("\n", '$NEWLINE')}:"
+          @last_error_msg << "#{Regexp.last_match(1).gsub("\n", '$NEWLINE')}:"
           @last_error_msg << data.lines.to_a[parser.failure_line - 1]
           @last_error_msg << "#{'~' * (parser.failure_column - 1)}^"
           puts @last_error_msg
