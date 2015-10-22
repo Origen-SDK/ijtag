22e169f48435b7c7e3dfc65d74171ada71feb8b6
Stephen McGinty <stephen.mcginty@freescale.com>
2015-10-22 07:27:01 -0500

    Added ast node create helpers

--- lib/ijtag/icl/node.rb
+++ lib/ijtag/icl/node.rb
@@ -1,7 +1,27 @@
 require 'ast'
+require 'treetop'
 module IJTAG
   module ICL
     class Node < ::AST::Node
+    end
+  end
+end
+
+# Some helpers to create the to_ast methods in syntax nodes
+module Treetop
+  module Runtime
+    class SyntaxNode
+      def n(type, children)
+        IJTAG::ICL::Node.new(type, children)
+      end
+
+      def n0(type)
+        n(type, [])
+      end
+
+      def n1(type, arg)
+        n(type, [arg])
+      end
     end
   end
 end
